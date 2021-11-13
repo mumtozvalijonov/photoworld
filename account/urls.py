4f1e16d54397c6865b9c5adf606afcd12cbadd47
Mumtoz Valijonov <mumtozvalijonov@gmail.com>
2021-11-13 17:31:53 +0500

    Add article model

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
+
 
 from rest_framework.routers import SimpleRouter
 from account import views
@@ -8,4 +10,7 @@ router = SimpleRouter()
 router.register('', views.AccountViewSet)
 
 
-urlpatterns = [] + router.get_urls()
+urlpatterns = [
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh')
+] + router.get_urls()
